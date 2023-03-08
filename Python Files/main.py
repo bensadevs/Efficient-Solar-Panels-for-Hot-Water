84aedaeaa28fd60e43ce96d8203feb2284f0f04c
moibensa <moises@bu.edu>
2023-03-08 11:24:21 -0500

    log file

--- Python Files/main.py
+++ Python Files/main.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 from flask import Flask, render_template_string
 
 app = Flask(__name__)
-matplotlib.use('Agg')
 
-# Read the CSV file into a Pandas DataFrame
-df = pd.read_csv('log.csv', names=['time', 'voltage', 'current', 'temperature'])
+matplotlib.use('Agg')
+filename= 'PANELLOG.csv'
+# filename = '/Volumes/SOLAR PANEL/LOG.CSV'
+df = pd.read_csv(filename, names=['time', 'voltage', 'current', 'temperature'])
+print(df)
 
 def generate_graph_html(title, xlabel, ylabel, xdata, ydata):
     fig, ax = plt.subplots()
@@ -18,40 +20,35 @@ def generate_graph_html(title, xlabel, ylabel, xdata, ydata):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
-    # Save the graph as a PNG image and encode it as a Base64 string
     buffer = BytesIO()
     fig.savefig(buffer, format='png')
     buffer.seek(0)
-    image_string = base64.b64encode(buffer.getvalue()).decode()
+    encoded_image = base64.b64encode(buffer.getvalue()).decode()
 
-    # Generate the HTML code for the graph
     html = '''
     <div class="graph-container">
         <h3>{title}</h3>
-        <img src="data:image/png;base64,{image_string}">
+        <img src="data:image/png;base64,{encoded_image}">
     </div>
     '''
-    return html.format(title=title, image_string=image_string)
+    return html.format(title=title, encoded_image=encoded_image)
 
 def panel_summary(power_generated, cost):
-    return ["Panel "+str(i+1) + ": Generated" +str(power_generated[i]) +"W today. Cost: "+"${:.2f}".format(cost[i]) for i in range(0,len(power_generated))]
+    return ["Panel "+str(i+1) + ": Generated " +str(power_generated[i]) +"W today. Cost: "+"${:.2f}".format(cost[i]) for i in range(0,len(power_generated))]
 
-# Define a Flask route to display the webpage
+
 @app.route('/')
 def display_page():
-    # Generate the HTML code for each graph
     graphs_html = [
         generate_graph_html('Temperature vs Time', 'Time (s)', 'Temperature (C)', df['time'], df['temperature']),
         generate_graph_html('Voltage vs Time', 'Time (s)', 'Voltage (V)', df['time'], df['voltage']),
         generate_graph_html('Current vs Time', 'Time (s)', 'Current (A)', df['time'], df['current'])
     ]
-
     panel_list = panel_summary([200,300,100],[20,30,10])
-    print(panel_list)
     
     boston_temp = 45
 
-    template_string = '''
+    html_template = '''
     <!DOCTYPE html>
     <html>
     <head>
@@ -64,11 +61,9 @@ def display_page():
         height: 100%
         align-items: center;
         background-color: #494949;
-        /* Add a hover effect */
         transition: box-shadow 0.3s ease-in-out;
         }
 
-        /* Styles for the graph and list on the right */
         .right-container {
         display: flex;
         flex-direction: column;
@@ -78,14 +73,12 @@ def display_page():
         width: 50%;
         margin: 20px;
         background-color: #A9A9A9;
-        /* Add a hover effect */
         transition: box-shadow 0.3s ease-in-out;
         }
         .right-container:hover {
         box-shadow: 3px 3px #333;
         }
 
-        /* Styles for the list and weather on the left */
         .left-container {
         display: flex;
         flex-direction: column;
@@ -95,7 +88,6 @@ def display_page():
         width: 50%;
         margin: 20px;
         background-color: #A9A9A9;
-        /* Add a hover effect */
         transition: box-shadow 0.3s ease-in-out;
         }
         .graph-container:hover {
@@ -111,7 +103,6 @@ def display_page():
         background-color: #fff;
         }
 
-        /* Styles for the rectangle on the bottom left */
         .weather-container {
         width: 100%;
         height: 50%;
@@ -145,9 +136,8 @@ def display_page():
     </body>
     </html>
 
-
     '''
-    return render_template_string(template_string, graphs=''.join(graphs_html),temp = boston_temp, panel_data = '<br></br>'.join(panel_list))
+    return render_template_string(html_template, graphs=''.join(graphs_html),temp = boston_temp, panel_data = '<br></br>'.join(panel_list))
 
 if __name__ == '__main__':
     app.run(debug=True)
